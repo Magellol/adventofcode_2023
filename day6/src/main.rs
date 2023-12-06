970b268469de69b1a0bc29a3e964cc4ad4cbcf32
Thomas Lefebvre <thmslbvr@gmail.com>
2023-12-06 08:00:53 -0500

    Day 6 part 1

---
+++ day6/src/main.rs
@@ -0,0 +1,38 @@
+use lib::lines_from_file;
+
+fn day6(input: Vec<String>) -> i32 {
+    let races = [(60, 601), (80, 1163), (86, 1559), (76, 1300)];
+
+    races.iter().fold(1, |acc, (time, distance)| {
+        let range = 0..*time + 1;
+
+        let result = range.fold(0, |acc2, ms| {
+            let remaining = time - ms;
+            let current_distance = ms * remaining;
+
+            if &current_distance > distance {
+                acc2 + 1
+            } else {
+                acc2
+            }
+        });
+
+        acc * result
+    })
+}
+
+#[test]
+fn day6_test() {
+    let input = vec![
+        "Time:      7  15   30".to_string(),
+        "Distance:  9  40  200".to_string(),
+    ];
+
+    assert_eq!(day6(input), 288)
+}
+
+fn main() {
+    let contents = lib::lines_from_file("./day6/input.txt");
+
+    println!("{}", day6(contents));
+}
